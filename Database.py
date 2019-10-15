8e64640717683c6f1440e538a73eb3d09ce787c0
Guilherme Ribeiro <guifrribeiro@gmail.com>
2019-10-15 01:12:04 -0300

    Create of database class to save data in memory and create of Inverted Index class

---
+++ Database.py
@@ -0,0 +1,15 @@
+class Database:
+    def __init__(self):
+        self.db = dict()
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def get(self, id):
+        return self.db.get(id, None)
+
+    def add(self, document):
+        return self.db.update({document['id']: document})
+
+    def remove(self, document):
+        return self.db.pop(document['id'], None)
---
+++ InvertedIndex.py
@@ -0,0 +1,40 @@
+import re
+
+from Appearance import Appearance
+
+
+class InvertedIndex:
+    def __init__(self, db):
+        self.index = dict()
+        self.db = db
+
+    def __repr__(self):
+        return str(self.index)
+
+    def index_document(self, document):
+        """
+        Process a given document, save it to the DB and update the index
+        :param document:
+        :return:
+        """
+        # Remove punctuation from the text
+        clean_text = re.sub(r'[^\w\s]', '', document['text'])
+        terms = clean_text.split(' ')
+        appearances_dict = dict()
+
+        for term in terms:
+            term_frequency = appearances_dict[term].frequency if term in appearances_dict else 0
+            appearances_dict[term] = Appearance(document['id'], term_frequency + 1)
+
+        update_dict = {key: [appearance]
+                       if key not in self.index
+                       else self.index[key] + [appearance]
+                       for (key, appearance) in appearances_dict.items()}
+        self.index.update(update_dict)
+
+        self.db.add(document)
+
+        return document
+
+    def lookup_query(self, query):
+        return {term: self.index[term] for term in query.split(' ') if term in self.index}
